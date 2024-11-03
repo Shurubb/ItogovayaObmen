28ca28491a0452d6ecc8a846280486580c331e7b
Shurubb <sahskamak@mail.ru>
2024-11-03 22:28:34 +0300

    Программа курса валют

--- ItogovayaObmen.py
+++ ItogovayaObmen.py
@@ -1,8 +1,35 @@
+from pydoc import resolve
+
 import requests
 import json
 import pprint
+from tkinter import *
+from tkinter import messagebox as mb
 
-obmen = requests.get('https://open.er-api.com/v6/latest/USD')
-result = json.loads(obmen.text)
-ob = pprint.PrettyPrinter(indent=4)
-ob.pprint(result)
+from Scripts.pythonProject.venv.Scripts.bottle import response
+
+
+def exchange():
+    code = entry.get()
+    if code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo('Курс обмена', f'Курс:  {exchange_rate:.2f} {code} за 1 доллар')
+            else:
+                mb.showerror('Ошибка', f'Валюта {code} не найдена')
+        except Exception as e:
+            mb.showerror('Error', f'Произошла ошибка: {e}.')
+    else:
+        mb.showwarning('Attention!', 'Введите код валюты')
+
+
+window = Tk()
+window.title('Курсы обмена валют')
+window.geometry('150x150')
+Label(text='Введите код валюты').pack(pax=10, pady=10)
+entry = Entry()
+window.mainloop()
