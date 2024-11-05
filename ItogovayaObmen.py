826397446e3b28c22d359f431e092b9f5a9b171a
Shurubb <sahskamak@mail.ru>
2024-11-05 15:04:19 +0300

    В метку добавлены названия валют

--- ItogovayaObmen.py
+++ ItogovayaObmen.py
@@ -1,3 +1,5 @@
+from tkinter.ttk import Combobox
+
 import requests
 import json
 import pprint
@@ -5,6 +7,10 @@ from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import ttk
 
+def up_c_label(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
 
 def exchange():
     code = combobox.get()
@@ -15,7 +21,8 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo('Курс обмена', f'Курс:  {exchange_rate:.2f} {code} за 1 доллар')
+                c_name= cur[code]
+                mb.showinfo('Курс обмена', f'Курс:  {exchange_rate:.2f} {c_name} за 1 доллар')
             else:
                 mb.showerror('Ошибка', f'Валюта {code} не найдена')
         except Exception as e:
@@ -23,13 +30,29 @@ def exchange():
     else:
         mb.showwarning('Attention!', 'Введите код валюты')
 
+cur = {
+    'RUB': 'Российский рубль',
+    'EUR': 'Евро',
+    'CNY': 'Китайский юань',
+    'GBP': 'Фунт стерлингов',
+    'JPY': 'Японская йена',
+    'UZS': 'Узбекский сум',
+    'KZT': 'Казахский тенге',
+    'AED': 'Арабский дирхам',
+    'CAD': 'Канадский доллар',
+    'CHF': 'Швейцарский франк'
+}
 
 window = Tk()
 window.title('Курсы обмена валют')
-window.geometry('150x150')
+window.geometry('250x200')
 Label(text='Выберите код валюты').pack(padx=10, pady=10)
-cur = ['RUB', 'EUR', 'CNY', 'GBP', 'GPY', 'UZS', 'KZT', 'AED', 'CAD', 'CHF']
-combobox = ttk.Combobox(values=cur)
+
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx = 10, pady = 10)
-# entry = Entry()
-# window.mainloop()
+combobox.bind('<<ComboboxSelected>>', up_c_label)
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
+Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
