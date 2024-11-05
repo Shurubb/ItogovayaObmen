2e551a41d1a3226c6a1d832c9f9580a12744d3ea
Shurubb <sahskamak@mail.ru>
2024-11-05 18:50:58 +0300

    Котики итог

--- Cats.py
+++ Cats.py
@@ -3,6 +3,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
+from tkinter import ttk
 
 
 
@@ -19,7 +20,10 @@ def get_image(url_api):
         return None
 
 def set_image():
-    img = get_image(url)
+    tag = i_tag.get()
+    url = 'https://cataas.com/cat'
+    url_tag = f'https://cataas.com/cat/{tag}'
+    img = get_image(url_tag if tag else  url)
     if img:
         new_window = Toplevel()
         new_window.title('Котики')
@@ -31,13 +35,15 @@ def set_image():
 
 def exit_win():
     window.destroy()
-
-url = 'https://cataas.com/cat'
+#Теги котов
+sp_tags = ['sleep', 'orange', 'black', 'smile', 'cute']
 
 window = Tk()
 window.geometry('300x200')
 window.title('Гланвое окно')
 
+
+#Добавление меню
 main_menu = Menu(window)
 window.config(menu=main_menu)
 file_menu = Menu(main_menu, tearoff=0)
@@ -46,7 +52,10 @@ main_menu.add_cascade(label='Файл', menu=file_menu)
 file_menu.add_command(label='Получить котика', command=set_image)
 file_menu.add_separator()
 file_menu.add_command(label='Выйти', command=exit_win)
+lab = Label(window, text='Выберите тип котика').pack
 
-
+#Добавление выпадающего списка
+i_tag = ttk.Combobox(window, values=sp_tags)
+i_tag.pack()
 
 window.mainloop()
